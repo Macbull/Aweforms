db358ae6884b553697c9f605ea82f45a19993aca
Vishwajeet Narwal <vnarwal95@gmail.com>
2017-03-08 16:08:25 -0600

    pep8 formatting of python

--- aweform.py
+++ aweform.py
@@ -11,15 +11,19 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-    '''Serve Static file index.html. Rest of the routing will be done by ngRoute'''
+    '''Serve Static file index.html. Further routing will be done by ngRoute'''
+
     return app.send_static_file('index.html')
+
 
 @app.route('/proxy/<path:url>')
 def home(url):
     '''Serve google form page using proxy method due to cross origin policy'''
-    req = requests.get(url, stream = True)
-    rreq = re.sub(r'https://(?!(fonts.googleapis)|(docs.google))', '/proxy/https://',req.content, flags=re.IGNORECASE)
-    return Response(rreq, content_type = req.headers['content-type'])
+    req = requests.get(url, stream=True)
+    rreq = re.sub(r'https://(?!(fonts.googleapis)|(docs.google))',
+                  '/proxy/https://', req.content, flags=re.IGNORECASE)
+    return Response(rreq, content_type=req.headers['content-type'])
+
 
 if __name__ == '__main__':
     app.run()
