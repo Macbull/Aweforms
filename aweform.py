1e66f857cb49f8f476692f3aa480bdf76b1ee958
Vishwajeet Narwal <vnarwal95@gmail.com>
2017-03-08 00:42:22 -0600

    Adding flask proxy server

---
+++ aweform.py
@@ -0,0 +1,25 @@
+from flask import Flask, Response
+import requests
+import logging
+import re
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger("aweform.py")
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    '''Serve Static file index.html. Rest of the routing will be done by ngRoute'''
+    return app.send_static_file('index.html')
+
+@app.route('/proxy/<path:url>')
+def home(url):
+    '''Serve google form page using proxy method due to cross origin policy'''
+    req = requests.get(url, stream = True)
+    rreq = re.sub(r'https://(?!(fonts.googleapis)|(docs.google))', '/proxy/https://',req.content, flags=re.IGNORECASE)
+    return Response(rreq, content_type = req.headers['content-type'])
+
+if __name__ == '__main__':
+    app.run()
