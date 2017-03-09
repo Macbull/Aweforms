3087b6a21aa354e024d5d93dca8c237b3bbbe5b5
Vishwajeet Narwal <vnarwal95@gmail.com>
2017-03-08 23:58:58 -0600

    /forms route and /proxy route

--- aweform.py
+++ aweform.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response
-import requests
+import urllib2
 import logging
 import re
 logging.basicConfig(level=logging.INFO)
@@ -15,15 +15,24 @@ def index():
 
     return app.send_static_file('index.html')
 
+@app.route('/forms/<path:url>')
+def forms(url):
+    '''Serve google form page using proxy method due to cross origin policy'''
+    page = urllib2.urlopen('https://docs.google.com/forms/'+url)
+    mod_page = re.sub(r'https://(?!(fonts.googleapis)|(docs.google))',
+                  '/proxy/', page.read(), flags=re.IGNORECASE)
+    return Response(mod_page, content_type=page.headers['content-type'])
+
+
 
 @app.route('/proxy/<path:url>')
-def home(url):
+def scripts(url):
     '''Serve google form page using proxy method due to cross origin policy'''
-    req = requests.get(url, stream=True)
-    rreq = re.sub(r'https://(?!(fonts.googleapis)|(docs.google))',
-                  '/proxy/https://', req.content, flags=re.IGNORECASE)
-    return Response(rreq, content_type=req.headers['content-type'])
+    page = urllib2.urlopen('https://'+url)
+    mod_page = re.sub(r'https://(?!(fonts.googleapis)|(docs.google))',
+                  '/proxy/', page.read(), flags=re.IGNORECASE)
+    return Response(mod_page, content_type=page.headers['content-type'])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(Threaded=True)
