ab7277edf2e4605eb76fb1efce7368bfaa8265a8
Vishwajeet Narwal <vnarwal95@gmail.com>
2017-03-14 01:30:03 -0500

    mend

--- aweform.py
+++ aweform.py
@@ -10,14 +10,14 @@ LOG = logging.getLogger("aweform.py")
 app = Flask(__name__, static_url_path='')
 
 
-# @app.before_request
-# def redirect_nonwww():
-#     """Redirect non-www requests to www."""
-#     urlparts = urlparse(request.url)
-#     if urlparts.netloc not in ['www.aweforms.com', '']:
-#         urlparts_list = list(urlparts)
-#         urlparts_list[1] = 'www.aweforms.com'
-#         return redirect(urlunparse(urlparts_list), code=301)
+@app.before_request
+def redirect_nonwww():
+    """Redirect non-www requests to www."""
+    urlparts = urlparse(request.url)
+    if urlparts.netloc not in ['www.aweforms.com', '']:
+        urlparts_list = list(urlparts)
+        urlparts_list[1] = 'www.aweforms.com'
+        return redirect(urlunparse(urlparts_list), code=301)
 
 @app.route('/')
 def index():
