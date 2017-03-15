a2cd1c214c4bfe5eac1b632d8ce8d2f6c8c6708f
Vishwajeet Narwal <vnarwal95@gmail.com>
2017-03-15 04:43:25 -0500

    // Test adding ng-model on first element : textField Worked

--- static/js/form/controllers/formController.js
+++ static/js/form/controllers/formController.js
@@ -5,20 +5,31 @@
   .module('app.form.controllers')
   .controller("formCtrl", formCtrl);
 
-  formCtrl.$inject = ["$scope", "$location", "Form", "Global", "currentAuth", "$routeParams", "$firebaseObject","Thumb"];
+  formCtrl.$inject = ["$scope", "$compile", "$location", "Form", "Global", "currentAuth", "$routeParams", "$firebaseObject","Thumb"];
 
 
   /**
   * @namespace formCtrl
   **/
-  function formCtrl($scope, $location, Form, Global, currentAuth, $routeParams, $firebaseObject, Thumb) {
+  function formCtrl($scope, $compile, $location, Form, Global, currentAuth, $routeParams, $firebaseObject, Thumb) {
     var vm = this;
     Form.destroyData();
     var form = Form.getData(currentAuth, $routeParams.param);
     form.$bindTo($scope,"newForm");
-  
+    var iframeDoc;
+    document.getElementById("iframeID").onload = function(){
+      iframeDoc = this.contentWindow.document;
+      $scope.newForm.name = iframeDoc.getElementsByClassName('freebirdFormviewerViewHeaderTitle')[0].innerText;
+      var iframeForm = iframeDoc.getElementsByTagName('form');
+      var iframeInputs = iframeDoc.getElementsByClassName("quantumWizTextinputPaperinputInput");
 
-
+// Test adding ng-model on first element
+      var mo = 'newForm.'+iframeInputs[0].getAttribute('jsname');
+      console.log(iframeForm[0]);
+      iframeInputs[0].setAttribute('ng-model',mo);
+      iframeInputs[0].setAttribute('ng-model-options','{ allowInvalid: true }');
+      $compile(iframeInputs[0])($scope);
+    }
 
 
 
