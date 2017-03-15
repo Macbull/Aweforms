a3cf07a24c876671cb720a1b6d2add84e2aeb9e8
Vishwajeet Narwal <vnarwal95@gmail.com>
2017-03-15 02:29:08 -0500

    Adding Thumbnail service for firebaseStorage, adding new logo

--- static/js/form/controllers/formController.js
+++ static/js/form/controllers/formController.js
@@ -5,17 +5,21 @@
   .module('app.form.controllers')
   .controller("formCtrl", formCtrl);
 
-  formCtrl.$inject = ["$scope", "$location", "Form", "Global", "currentAuth", "$routeParams", "$firebaseObject"];
+  formCtrl.$inject = ["$scope", "$location", "Form", "Global", "currentAuth", "$routeParams", "$firebaseObject","Thumb"];
 
 
   /**
   * @namespace formCtrl
   **/
-  function formCtrl($scope, $location, Form, Global, currentAuth, $routeParams, $firebaseObject) {
+  function formCtrl($scope, $location, Form, Global, currentAuth, $routeParams, $firebaseObject, Thumb) {
     var vm = this;
     Form.destroyData();
     var form = Form.getData(currentAuth, $routeParams.param);
     form.$bindTo($scope,"newForm");
+  
+
+
+
 
 
   }
